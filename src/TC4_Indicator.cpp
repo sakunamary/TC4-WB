23a764ee3805e209b7b0b47106e1c9a745997509
Tim <sakunamary@gmail.com>
2022-09-19 14:44:23 +0800

    update
    update

--- src/TC4_Indicator.cpp
+++ src/TC4_Indicator.cpp
@@ -17,6 +17,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "img.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
