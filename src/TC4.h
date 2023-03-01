bfb760182121c31b762c779bae2ac78ffea25496
Tim <sakunamary@gmail.com>
2023-03-01 17:09:50 +0800

    update

--- src/TC4.h
+++ src/TC4.h
@@ -12,8 +12,7 @@
 #define __TC4_H__
 
 #define PRINT_ARTISAN_WHOLE_MESSAGE 1     // set, to print Artisan commands on serial debug port
-#define PRINT_TEAMPERATURE_EACH_READING 1 // Set, to print temperature vaule on serial debug port
-
+#define TEMPERATURE_ARRAY_LENGTH    10
 
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
 #define BAUDRATE 57600  //serial port baudrate
