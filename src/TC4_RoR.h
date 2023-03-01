a27fe13caf59dd74bf94b1620b3ed09190bd16de
Tim <sakunamary@gmail.com>
2023-03-01 17:09:50 +0800

    update

--- src/TC4_RoR.h
+++ src/TC4_RoR.h
@@ -6,12 +6,13 @@
 
 //extern float BT_AvgTemp;
 //extern float ET_CurTemp;
-float ROR_TempArray[TEMPERATURE_ARRAY_LENGTH] = {0.0}; // temperature array
+float BT_ROR_TempArray[TEMPERATURE_ARRAY_LENGTH] = {0.0}; // temperature array
+float ET_ROR_TempArray[TEMPERATURE_ARRAY_LENGTH] = {0.0}; // temperature array
 
 float Temp_last ;
 //float BT_ROR = 0.0;
 //float ET_ROR = 0.0;
-
+int ArrayIndex = 0;
 float ROR_Temp(float Temp_in,float count_time);
 
 void TaskROR(void *pvParameters)
@@ -20,7 +21,7 @@ void TaskROR(void *pvParameters)
      /* Variable Definition */
     (void)pvParameters;
     TickType_t xLastWakeTime;
-    const TickType_t xIntervel = (user_wifi.sampling_time * 1000) / portTICK_PERIOD_MS;
+    const TickType_t xIntervel = (user_wifi.sampling_time * 1000) / portTICK_PERIOD_MS; //every 5s get a data
 
         // Initial the xLastWakeTime variable with the current time.
     xLastWakeTime = xTaskGetTickCount();
@@ -31,10 +32,17 @@ void TaskROR(void *pvParameters)
         vTaskDelayUntil(&xLastWakeTime, xIntervel);
        if (xSemaphoreTake(xThermoDataMutex, xIntervel) == pdPASS) 
        {
+
         temperature_data.BT_ROR = ROR_Temp(temperature_data.BT_AvgTemp  ,4);
         temperature_data.ET_ROR = ROR_Temp(temperature_data.ET_CurTemp ,4);
         xSemaphoreGive(xThermoDataMutex);  //end of lock mutex
        }
+
+
+
+
+
+
 
     }
 }
